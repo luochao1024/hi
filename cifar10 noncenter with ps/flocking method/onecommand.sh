7d0d236fa4261a76d79383808373419c164a5eb1
Luochao Wang <luochao@wang.com>
2018-05-16 12:57:11 -0500

    add log files

--- cifar10 noncenter with ps/flocking method/onecommand.sh
+++ cifar10 noncenter with ps/flocking method/onecommand.sh
@@ -1,16 +1,18 @@
 w=3
 t=$((w-1))
-taskset -c $w-$((w+31)) python3 cnn_flocking_method.py --job_name ps --task_index 0 &
-for ((x=0; x<$t; x++))
-do
-    taskset -c $x python3 cnn_flocking_method.py  --job_name worker --task_index $x &#>/dev/null &
-done
-
-taskset -c $t python3 cnn_flocking_method.py --job_name worker --task_index $t
+taskset -c 12-30 python3 cnn_flocking_method.py --job_name ps --task_index 0 &
+#taskset -c $w-$((w+31)) python3 cnn_flocking_method.py --job_name ps --task_index 0 &
+#for ((x=0; x<$t; x++))
+#do
+#    taskset -c $x python3 cnn_flocking_method.py  --job_name worker --task_index $x &#>/dev/null &
+#done
+#
+#taskset -c $t python3 cnn_flocking_method.py --job_name worker --task_index $t
 
 #taskset -c 2 python3 test_task0.py  --task_index 2 &
 #taskset -c 1 python3 test_task0.py  --task_index 1 &
 #taskset -c 0 python3 test_task0.py  --task_index 0
 
-#taskset -c 0 python3 cnn_flocking_method.py --task_index 0 &
-#taskset -c 1 python3 cnn_flocking_method.py --task_index 1
+taskset -c 0-3 python3 cnn_flocking_method.py --job_name worker --task_index 0 &
+taskset -c 4-7 python3 cnn_flocking_method.py --job_name worker --task_index 1 &
+taskset -c 8-11 python3 cnn_flocking_method.py --job_name worker --task_index 2
