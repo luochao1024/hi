0ea0d309f85fb90c265abe41eb904cccdb71ad45
Luochao Wang <luochao@wang.com>
2018-05-16 12:14:12 -0500

    add log files

--- cifar10 noncenter with ps/flocking method/start.sh
+++ cifar10 noncenter with ps/flocking method/start.sh
@@ -1,10 +1,4 @@
-w=4
-a=$((w-1))
+git pull origin master
+source onecommand.sh
 
-python3 cnn_flocking_method.py --job_name ps --task_index 0 &>/dev/null &
-for ((x=0; x<$a; x++))
-do
-    python3 cnn_flocking_method.py --job_name worker --task_index $x &>/dev/null &
-done
 
-python3 cnn_flocking_method.py --job_name worker --task_index $a
