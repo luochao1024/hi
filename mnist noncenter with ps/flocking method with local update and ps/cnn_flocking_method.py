2655d0e65f427d402947143556118f7078e60cfc
Luochao Wang <luochao@wang.com>
2018-05-17 10:58:30 -0500

    add log files

--- mnist noncenter with ps/flocking method with local update and ps/cnn_flocking_method.py
+++ mnist noncenter with ps/flocking method with local update and ps/cnn_flocking_method.py
@@ -13,8 +13,8 @@ import cnn_tower_with_ps as cnn_tower
 BATCH_SIZE = 64
 NUM_WORKERS = 4
 NUM_FLOCKING_WORKERS = 3
-ATTRACTION = 0.001
-REPULSION = 0.005
+ATTRACTION = 0.000
+REPULSION = 0.000
 
 
 def main():
